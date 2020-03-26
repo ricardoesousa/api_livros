0c82f06263d729a34b6820a91d020030b4787f38
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-03-26 20:54:07 -0300

    2.10 Tratamento correto das respostas HTTP 404 e 201

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -1,13 +1,17 @@
 package com.algaworks.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
@@ -25,14 +29,23 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		livrosRepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+		livro = livrosRepository.save(livro);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(livro.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Livro buscar(@PathVariable("id") Long id) {
-		return livrosRepository.findById(id).get();
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Livro livro = livrosRepository.findById(id).orElse(null);
 		
+		if(livro == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)

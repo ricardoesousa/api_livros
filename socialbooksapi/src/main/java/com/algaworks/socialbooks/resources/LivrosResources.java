29446d7b40d5c0f3e51977cbe742a697595582ff
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-03-26 19:26:52 -0300

    2.5 Interagindo com o banco de dados

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -1,23 +1,23 @@
 package com.algaworks.socialbooks.resources;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.repository.LivrosRepository;
 
 @RestController
 public class LivrosResources {
+	
+	@Autowired
+	private LivrosRepository livrosRepository;
 
 	@RequestMapping(value = "/livros", method = RequestMethod.GET)
 	public List<Livro> listar() {
-
-		Livro l1 = new Livro("Rest Aplicado");
-		Livro l2 = new Livro("Git passo-a-passo");
-		Livro[] livros = { l1, l2 };
-		return Arrays.asList(livros);
+		return livrosRepository.findAll();
 	}
 }

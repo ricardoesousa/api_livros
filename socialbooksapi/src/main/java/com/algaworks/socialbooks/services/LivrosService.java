1183b7b3d08209cc83165976c8702042e41fb1b0
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-03-30 12:00:27 -0300

    2.15 Listando os comentários do recurso Livro

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/services/LivrosService.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/services/LivrosService.java
@@ -64,4 +64,10 @@ public class LivrosService {
 		
 		return comentariosRepository.save(comentario);
 	}
+	
+	public List<Comentario> listarComentarios(Long livroId) {
+		Livro livro = buscar(livroId);
+		
+		return livro.getComentarios();
+	}
 }

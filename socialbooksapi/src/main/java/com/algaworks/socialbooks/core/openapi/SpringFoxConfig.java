234db70b6aabf2fc21d0d49b75fc305d1f2d2c14
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-04-03 07:50:54 -0300

    5.Selecionando os endpoints da API para gerar a documentação

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/core/openapi/SpringFoxConfig.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/core/openapi/SpringFoxConfig.java
@@ -12,21 +12,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig implements WebMvcConfigurer{
-	
+public class SpringFoxConfig implements WebMvcConfigurer {
+
 	@Bean
 	public Docket apiDocket() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.algaworks.socialbooks.resources")).build();
 	}
-	
+
 	@Override
-	public void addResourceHandlers (ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html")
-		.addResourceLocations("classpath:/META-INF/resources/");
-	
-		registry.addResourceHandler("/webjars/**")
-		.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
-	
 
 }
